e7adc861a05583b0ae415dfa598de706df778d22
Michaellee955 <minghao95@outlook.com>
2017-11-21 23:21:32 -0500

    IO

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class SimpleIO {
+	public static void main(String[] arg) throws Exception {
+		BufferedReader Keyboard = new BufferedReader(new InputStreamReader(
+			System.in));
+		System.out.println(Keyboard.readLine());
+	}
+}
